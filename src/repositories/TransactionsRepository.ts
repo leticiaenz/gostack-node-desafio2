9e942cf1cd2769046f6a1dd17eda2d20487b4dbb
leticiaenz <leticia.enz11@gmail.com>
2020-04-16 15:44:47 -0400

    creates and lists transactions without SoC

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,10 +1,10 @@
 import Transaction from '../models/Transaction';
 
-interface Balance {
-  income: number;
-  outcome: number;
-  total: number;
-}
+// interface Balance {
+//   income: number;
+//   outcome: number;
+//   total: number;
+// }
 
 class TransactionsRepository {
   private transactions: Transaction[];
@@ -14,15 +14,18 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
-  public getBalance(): Balance {
-    // TODO
-  }
+  // public getBalance(): Balance {
+  //   // TODO
+  // }
 
-  public create(): Transaction {
-    // TODO
+  public create(title: string, value: number, type: string): Transaction {
+    const transaction = new Transaction(title, value, type);
+
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,26 +1,36 @@
 import { Router } from 'express';
-
-// import TransactionsRepository from '../repositories/TransactionsRepository';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
 // import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
-  try {
-    // TODO
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  const transactions = transactionsRepository.all();
+
+  return response.json(transactions);
+
+  // try {
+  //   // TODO
+  // } catch (err) {
+  //   return response.status(400).json({ error: err.message });
+  // }
 });
 
 transactionRouter.post('/', (request, response) => {
-  try {
-    // TODO
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  // try {
+  //   // TODO
+  // } catch (err) {
+  //   return response.status(400).json({ error: err.message });
+  // }
+
+  const { title, value, type } = request.body;
+
+  const transaction = transactionsRepository.create(title, value, type);
+
+  return response.json(transaction);
 });
 
 export default transactionRouter;
