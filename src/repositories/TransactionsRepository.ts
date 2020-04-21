f97a0fc6d38ae6c0d2f1e76abd233ca713ae9190
leticiaenz <leticia.enz11@gmail.com>
2020-04-21 13:53:24 -0400

    Create list  transaction

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -2,17 +2,20 @@ import Transaction from '../models/Transaction';
 
 interface CreateTransactionDTO {
   title: string;
-
   value: number;
-
   type: 'income' | 'outcome';
 }
 
-// interface Balance {
-//   income: number;
-//   outcome: number;
-//   total: number;
-// }
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+interface AllTransactions {
+  transactions: Transaction[];
+  balance: Balance;
+}
 
 class TransactionsRepository {
   private transactions: Transaction[];
@@ -21,11 +24,35 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    return this.transactions;
+  public all(): AllTransactions {
+    // pega os valores do balanço
+    const balance = this.getBalance();
+    const allData = {
+      transactions: this.transactions,
+      balance: balance,
+    };
+
+    return allData;
   }
 
-  // public getBalance({ income, outcome, total }: Balance) {}
+  public getBalance(): Balance {
+    let income = 0;
+    let outcome = 0;
+
+    this.transactions.map(function (item) {
+      if (item.type === 'income') {
+        income = income + item.value;
+      } else {
+        outcome = outcome + item.value;
+      }
+    });
+
+    return {
+      income: income,
+      outcome: outcome,
+      total: income - outcome,
+    };
+  }
 
   public create({ title, value, type }: CreateTransactionDTO): Transaction {
     const transaction = new Transaction({ title, value, type });
